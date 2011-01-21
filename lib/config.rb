04eef4aed2932ff13953cef7f56b5fd71049d403
Jim Bancroft <jbancroft@wistia.com>
2011-01-21 18:38:16 -0500

    Updated the README to include config instructions.

--- lib/config.rb
+++ lib/config.rb
@@ -1,9 +1,7 @@
 require 'configatron'
 
-#configatron.wistia.api.key = ''
-configatron.wistia.api.key = 'test'
+configatron.wistia.api.key = ''
 configatron.wistia.api.user = 'api'
 configatron.wistia.api.format = :json
-#configatron.wistia.api.url = 'https://api.wistia.com/v1/'
-configatron.wistia.api.url = 'http://api.wistia.local:3000/v1/'
+configatron.wistia.api.url = 'https://api.wistia.com/v1/'
 
