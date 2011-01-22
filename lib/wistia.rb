d4eca01b04ea0c0c4d8921963a6280356ea82251
Jim Bancroft <jbancroft@wistia.com>
2011-01-22 14:50:55 -0500

    Fixed it so you only have to specify your api password once.

--- lib/wistia.rb
+++ lib/wistia.rb
@@ -3,3 +3,10 @@ require 'active_resource'
 require 'wistia/base'
 require 'wistia/media'
 require 'wistia/project'
+
+module Wistia
+  def self.password=(pw)
+    Wistia::Media.password = pw
+    Wistia::Project.password = pw
+  end
+end
