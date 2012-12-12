6d7407e3ab42de73801910a5905539bd76aee04e
Adrian Mowat <adrian.mowat@arnoldclark.co.uk>
2012-12-12 14:32:40 +0000

    Monkey Patched Psych under jruby 1.7+ to address an unpatched issue in configatron

--- lib/config.rb
+++ lib/config.rb
@@ -1,3 +1,15 @@
+# Workaround for a problem with configatron in JRuby 1.7+
+# Contributors have suggested a solution at
+# https://github.com/markbates/configatron/pull/33
+# but the patch has not been applied for months
+
+if RUBY_PLATFORM == 'java' && !defined? Psych::Yecht
+  module Psych
+    module Yecht
+      MergeKey = Psych::Syck
+    end
+  end
+end
 require 'configatron'
 
 configatron.wistia.api.key = ''
