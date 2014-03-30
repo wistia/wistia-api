657822c6762ee858b1203d4a60121afee3cf4b9f
Ben A. Morgan <benjaminamorgan@hotmail.com>
2014-03-30 01:53:55 -0700

    added next video, previous video, and permalink

--- lib/wistia/media.rb
+++ lib/wistia/media.rb
@@ -14,5 +14,25 @@ module Wistia
 
       nil
     end
+
+    def previous_video(collection)
+      media_id_array = collection.map(&:id)
+
+      media_id_hash = Hash[media_id_array.map.with_index.to_a]
+      
+      collection[media_id_hash[self.id] - 1]
+    end
+
+    def next_video(collection)
+      media_id_array = collection.map(&:id)
+
+      media_id_hash = Hash[media_id_array.map.with_index.to_a]
+      
+      collection[media_id_hash[self.id] + 1]
+    end
+
+    def permalink
+      self.name.downcase.gsub(" ", "-")
+    end
   end
 end
