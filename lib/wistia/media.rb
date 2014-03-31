52f5ab88fd1d5ee01a33ee5168086aacfea5704c
Ben A. Morgan <benjaminamorgan@hotmail.com>
2014-03-30 19:45:03 -0700

    refactored code and added uri encoding permalink

--- lib/wistia/media.rb
+++ lib/wistia/media.rb
@@ -16,23 +16,22 @@ module Wistia
     end
 
     def previous_video(collection)
-      media_id_array = collection.map(&:id)
-
-      media_id_hash = Hash[media_id_array.map.with_index.to_a]
-      
-      collection[media_id_hash[self.id] - 1]
+      collection[media_hash(collection)[self.id] - 1]
     end
 
     def next_video(collection)
-      media_id_array = collection.map(&:id)
-
-      media_id_hash = Hash[media_id_array.map.with_index.to_a]
-      
-      collection[media_id_hash[self.id] + 1]
+      collection[media_hash(collection)[self.id] + 1]
     end
 
     def permalink
-      self.name.downcase.gsub(" ", "-")
+      URI::encode(self.name.downcase.gsub(" ", "-"))
+    end
+
+    private
+
+    def media_hash(collection)
+      media_id_array = collection.map(&:id)
+      Hash[media_id_array.map.with_index.to_a]
     end
   end
 end
